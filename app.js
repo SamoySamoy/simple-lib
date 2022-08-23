980199bfaf2e2a0f5c527ef1a359f21973cb7422
SamoySamoy <antalois23@gmail.com>
2022-08-23 23:50:04 +0700

    add changes required for deployment to heroku

--- app.js
+++ app.js
@@ -7,12 +7,17 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const catalogRouter = require('./routes/catalog'); //Import routes for "catalog" area of site
+const compression = require('compression');
+const helmet = require('helmet');
+
+
 
 const app = express();
 
 // Set up mongoose connection
 const mongoose = require('mongoose');
-const mongoDB = 'mongodb+srv://Samoy:mongodbngngng232323@cluster0.qtudueq.mongodb.net/?retryWrites=true&w=majority';
+const dev_db_url = 'mongodb+srv://Samoy:mongodbngngng232323@cluster0.qtudueq.mongodb.net/?retryWrites=true&w=majority';
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
@@ -25,6 +30,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression()); //Compress all routes
+app.use(helmet());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
